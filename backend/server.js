0984e7b03e911e42ec5218b044513440dd029eff
Bill Cathey <billcathey@hotmail.com>
2022-04-10 09:40:15 -0400

    Create routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use('/api/goals', require('./routes/goalRoutes'))
+
 app.listen(port, () => console.log(
     `Server started on port ${port}`
 ))
