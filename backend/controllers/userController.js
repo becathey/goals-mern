bd75e8ae6898e516087baca574eb7969df1afc63
Bill Cathey <billcathey@hotmail.com>
2022-04-12 20:45:53 -0400

    Add Login User functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,7 +43,20 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message: 'Login user'})
+    const {email, password} = req.body
+    // Check for user email
+    const user = await User.findOne({email})
+    // Check for password
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
 })
 
 // @desc Get user data
