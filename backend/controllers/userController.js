ae99f32f2c89aa3dc95ef40ca8c859c3318008bc
Bill Cathey <billcathey@hotmail.com>
2022-04-12 22:46:45 -0400

    Add authorization to getMe route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,9 +63,14 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // @desc Get user data
 // @route GET /api/users/me
-// @access Public
+// @access Private
 const getMe = asyncHandler(async (req, res) => {
-    res.json({message: 'User data'})
+    const {_id, name, email} = await User.findById(req.user.id)
+    res.status(200).json({
+        id: _id,
+        name,
+        email
+    })
 })
 
 // Generate JWT
