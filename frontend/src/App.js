57f729fde28f4c3028a52becd1640645a431c1b5
Bill Cathey <billcathey@hotmail.com>
2022-04-14 20:56:26 -0400

    Set up routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
