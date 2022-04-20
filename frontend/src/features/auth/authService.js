3b91991956d377ad43f64668474680b87fcc65ed
Bill Cathey <billcathey@hotmail.com>
2022-04-20 12:24:24 -0400

    Add logout functionality

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -10,8 +10,14 @@ const register = async (userData) => {
     return response.data
 }
 
+// Logout user
+const logout = () => {
+    localStorage.removeItem('user')
+}
+
 const authService = {
-    register
+    register,
+    logout
 }
 
 export default authService
