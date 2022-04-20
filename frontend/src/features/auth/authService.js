cd602c1085b1675a3e145ab79239ddf7ea91422f
Bill Cathey <billcathey@hotmail.com>
2022-04-20 17:34:45 -0400

    Add login functionality

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -10,6 +10,15 @@ const register = async (userData) => {
     return response.data
 }
 
+// Login user
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+    if(response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+    return response.data
+}
+
 // Logout user
 const logout = () => {
     localStorage.removeItem('user')
@@ -17,7 +26,8 @@ const logout = () => {
 
 const authService = {
     register,
-    logout
+    logout,
+    login
 }
 
 export default authService
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import {toast} from 'react-toastify'
 import {FaSignInAlt} from 'react-icons/fa'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -7,6 +12,18 @@ function Login() {
         password: '',
     })
     const {email, password} = formData
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
+    useEffect(() => {
+        if(isError) {
+            toast.error(message)
+        }
+        if(isSuccess || user) {
+            navigate('/')
+        }
+        dispatch(reset())
+    }, [user, isError, isSuccess, message, navigate, dispatch])
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...prevState,
@@ -15,7 +32,16 @@ function Login() {
     }
     const onSubmit = (e) => {
         e.preventDefault()
+        const userData = {
+            email,
+            password,
+        }
+        dispatch(login(userData))
     }
+    if(isLoading) {
+        return <Spinner />
+    }
+
     return (
         <>
             <section className='heading'>
@@ -23,7 +49,7 @@ function Login() {
                 <p>Log in and start setting goals</p>
             </section>
             <section className='form'>
-                <form>
+                <form onSubmit={onSubmit}>
                     <div className='form-group'>
                         <input type='email' 
                             className='form-control'
