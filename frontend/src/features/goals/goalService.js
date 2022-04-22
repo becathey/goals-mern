bcc945ae99d166ff50825ab527e220595d70ed57
Bill Cathey <billcathey@hotmail.com>
2022-04-22 13:31:20 -0400

    Add Get All Goals functionality

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -12,7 +12,19 @@ const createGoal = async (goalData, token) => {
     return response.data
 }
 
+// Get user goals
+const getGoals = async (token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    const response = await axios.get(API_URL, config)
+    return response.data
+}
+
 const goalService = {
-    createGoal
+    createGoal,
+    getGoals
 }
 export default goalService
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,16 +1,30 @@
 import {useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 import GoalForm from '../components/GoalForm'
+import Spinner from '../components/Spinner'
+import {getGoals, reset} from '../features/goals/goalSlice'
 
 function Dashboard() {
+    const {isLoading, isError, message} = useSelector((state) => state.goals)
     const navigate = useNavigate()
+    const dispatch = useDispatch()
     const {user} = useSelector((state) => state.auth)
     useEffect(() => {
+        if(isError) {
+            console.log(message)
+        }
         if(!user) {
             navigate('/login')
         }
-    }, [user, navigate])
+        dispatch(getGoals())
+        return () => {
+            dispatch(reset())
+        }
+    }, [user, navigate, isError, message, dispatch])
+    if (isLoading) {
+        return <Spinner />
+    }
 
     return (
         <>
