53b55279fe6d727e8ee529fb15571ae1a08f4100
Bill Cathey <billcathey@hotmail.com>
2022-04-21 21:23:23 -0400

    Delete duplicate code getting user

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -34,14 +34,13 @@ const updateGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
-    const user = await User.findById(req.user.id)
     // Check for user
-    if(!user) {
+    if(!req.user) {
         res.status(401)
         throw new Error('User not found')
     }
     // Make sure logged in user matches goal user
-    if(goal.user.toString() !== user.id) {
+    if(goal.user.toString() !== req.user.id) {
         res.status(401)
         throw new Error('User not authorized')
     }
@@ -58,14 +57,13 @@ const deleteGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
-    const user = await User.findById(req.user.id)
     // Check for user
-    if(!user) {
+    if(!req.user) {
         res.status(401)
         throw new Error('User not found')
     }
     // Make sure logged in user matches goal user
-    if(goal.user.toString() !== user.id) {
+    if(goal.user.toString() !== req.user.id) {
         res.status(401)
         throw new Error('User not authorized')
     }
