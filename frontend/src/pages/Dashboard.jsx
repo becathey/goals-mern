5c1afb717a0d38fc155371a3c1c3edd3ef2fc2b1
Bill Cathey <billcathey@hotmail.com>
2022-04-22 17:46:26 -0400

    Add Display Goals functionality

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,9 +4,10 @@ import {useSelector, useDispatch} from 'react-redux'
 import GoalForm from '../components/GoalForm'
 import Spinner from '../components/Spinner'
 import {getGoals, reset} from '../features/goals/goalSlice'
+import GoalItem from '../components/GoalItem'
 
 function Dashboard() {
-    const {isLoading, isError, message} = useSelector((state) => state.goals)
+    const {isLoading, isError, message, goals} = useSelector((state) => state.goals)
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const {user} = useSelector((state) => state.auth)
@@ -33,6 +34,17 @@ function Dashboard() {
                 <p>Welcome {user && user.name}</p>
             </section>
             <GoalForm />
+            <section className='content'>
+                {goals.length > 0 ? (
+                    <div className='goals'>
+                        {goals.map((goal) => (
+                            <GoalItem key={goal._id} goal={goal} />
+                        ))}
+                    </div>
+                ) : (
+                    <h3>You have not set any goals!</h3>
+                )}
+            </section>
         </>
     )
 }
