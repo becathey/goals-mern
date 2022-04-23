654eb9573983b8c7704e2af24c12fd3430c3a80b
Bill Cathey <billcathey@hotmail.com>
2022-04-23 15:36:04 -0400

    Fine-tune styles on goal class

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -14,7 +14,7 @@ function GoalForm() {
         <section className="form">
             <form onSubmit={onSubmit}>
                 <div className="form-group">
-                    <label htmlFor="text">Goal</label>
+                    <label htmlFor="text">Goal:</label>
                     <input type='text' name='text' id='text' value={text} onChange={(e) => setText(e.target.value)} />
                 </div>
                 <div className="form-group">
